9bd5eaa23cea6d5b137aec6c0ef486ce82ac4da5
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-10-01 16:11:19 +0200

    <FIX> update cors allowed methods

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,19 +1,18 @@
 <?php
 
 return [
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    
+
     'allowed_methods' => ['*'],
-    
-    'allowed_origins' => ['*'],
-    
-    'allowed_origins_patterns' => [],
-    
+
+    'allowed_origins' => ['http://cryptoworld-frontend-accoao-b09413-72-60-186-210.traefik.me'],
+
     'allowed_headers' => ['*'],
-    
+
     'exposed_headers' => [],
-    
+
     'max_age' => 0,
-    
+
     'supports_credentials' => true,
 ];
